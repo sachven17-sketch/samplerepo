d2785d37631d203a217fad5f10a2aa1379c3f276
sachven17-sketch <sachven17@gamil.com>
2026-07-03 12:22:30 +0530

    created sachin2.c

---
+++ sachin2.c
@@ -0,0 +1,13 @@
+#include <stio.h>
+int mian()
+{
+    int n1,n2,sum;
+    printf("enter the number 1:");
+    scanf("%d",&n1);
+    printf("enter the number 2:");
+    scanf("%d", &n2);
+    sum = n1+n2;
+    printf("answer:%d",sum);
+    return o;
+
+}
